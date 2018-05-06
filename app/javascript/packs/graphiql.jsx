e0f22ebabe3b5f7145505538112836f83dd1c3ba
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2018-05-06 17:13:10 +0900

    Add graphiql

---
+++ app/javascript/packs/graphiql.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GraphiQL from 'graphiql';
+import fetch from 'isomorphic-fetch';
+import '../graphiql/style'
+
+function graphQLFetcher(graphQLParams) {
+  return fetch(window.location.origin + '/graphql', {
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(graphQLParams),
+  }).then(response => response.json());
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <div style={{height: '100vh'}}>
+      <GraphiQL fetcher={graphQLFetcher} />
+    </div>,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'root#index'
 
+  if Rails.env.development?
+    get :graphiql, controller: 'root'
+  end
+
   get '/auth/:provider/callback', to: 'sessions#create'
 end
