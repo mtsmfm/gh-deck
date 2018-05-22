e83257d5f13d3ca08419e9d4bb8aa1453b390b9b
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2018-05-23 02:32:13 +0900

    Remove dummy staff

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,6 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
-  # Add root-level fields here.
-  # They will be entry points for queries on your schema.
 
-  # TODO: remove me
   field :viewer, Types::UserType do
     description "User"
     resolve ->(obj, args, ctx) {
