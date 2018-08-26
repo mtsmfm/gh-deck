062c1aaca9291415895ae49f8de53eae1b3d3134
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2018-08-26 13:06:32 +0900

    Use find_or_initialize_by to update info on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env['omniauth.auth']
-    user = User.find_or_create_by!(provider: auth[:provider], uid: auth[:uid]) do |u|
+    user = User.find_or_initialize_by(provider: auth[:provider], uid: auth[:uid]).tap do |u|
       u.update!(login: auth[:extra][:raw_info][:login], image: auth[:info][:image], token: auth[:credentials][:token])
     end
     session[:user_id] = user.id
