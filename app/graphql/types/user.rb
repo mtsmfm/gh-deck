4b76b898bd7e3c4a6e75d370d0554d87fc467a44
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2018-08-26 23:01:09 +0900

    Make github_events field searchable

--- app/graphql/types/user.rb
+++ app/graphql/types/user.rb
@@ -3,9 +3,21 @@ class Types::User < Types::BaseObject
   global_id_field :id
   field :login, String, null: false
   field :image, String, null: false
-  field :github_events, [Types::GithubEvent], null: false
+  field :github_events, [Types::GithubEvent], null: false do
+    argument :types, [Types::GithubEvent::TypeEnum], required: false
+  end
 
-  def github_events
-    object.github_events.order(github_created_at: :desc).limit(20)
+  def github_events(types: [])
+    known_all_types = Types::GithubEvent::TypeEnum.values.keys - ['UnknownEvent']
+    known_search_types = known_all_types & types
+
+    scope = object.github_events if types.empty?
+    scope = object.github_events.where(github_type: known_search_types) unless known_search_types.empty?
+    if types&.include?('UnknownEvent')
+      unknown_events = object.github_events.where.not(github_type: known_all_types)
+      scope = scope ? scope.or(unknown_events) : unknown_events
+    end
+
+    scope.order(github_created_at: :desc).limit(20)
   end
 end
